f0e7753941083f748685e9c274c6cdc2c4ac9c9f
kevinolweb <kevin.oleary91@gmail.com>
2022-10-13 11:07:50 +0000

    Answer function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -31,13 +31,25 @@ function runGame(gameType){
     }
 }
 
-function addition(){
-
-}
-
 function displayAdditionQuestion(operand1,operand2){
     document.getElementById('operand1').textContent=operand1;
     document.getElementById('operand2').textContent=operand2;
     document.getElementById('operator').textContent='+';
+
+}
+
+function calculateAnswer(){
+    let operand1 = parseInt(document.getElementById('operand1').innerText)
+    let operand2 = parseInt(document.getElementById('operand2').innerText)
+    let operator = document.getElementById('operator').innerText)
+
+    if (operator === "+"){
+        return operand1 + operand2,"addition"
+    }
+    else{
+        alert `Unimplemented operator ${operator}.`
+        throw `Unimplemented operator ${operator}. Aborting.`
+    }
+
 
 }
